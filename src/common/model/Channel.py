8aa45b17f1dfda3087a07ef660627c7a5c208db3
loh1na <loh1nacococ@gmail.com>
2024-02-20 15:25:55 +0200

    Models

---
+++ src/common/model/Channel.py
@@ -0,0 +1,7 @@
+class Channel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_station = db.Column(db.Integer, db.ForeignKey('Station.id'), nullable=False)
+    occupancy = db.Column(db.Boolean, unique=False, nullable = False)
+    title = db.Column(db.String(120), unique=False, nullable=False)
+    price = db.Column(db.String(120), unique=False, nullable=False)
+
---
+++ src/common/model/UserCar.py
@@ -0,0 +1,6 @@
+class UserCar(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False))
+    carname = db.Column(db.String(200), unique=False, nullable=False)
+
+
