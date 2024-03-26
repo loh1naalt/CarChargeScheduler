de1f0e9158339a4aba3bc6ac70f8839538859437
loh1na <loh1nacococ@gmail.com>
2024-03-26 11:36:03 +0200

    Changelog is in Main.py file

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,14 @@
-from flask import render_template, Flask, request, redirect, url_for
-from common.model.Models import Users, Station, Channel, db
+from flask import render_template, Flask, request, redirect
+from common.model.Models import Users, Station, Channel, UserCar, db
 from sqlalchemy.sql import text
+import common.service.CrudHelper as CrudHelper
 
 # changelog
 # User.py will be merged to Admin.py, hence, Admin.py will be renamed to Main.py. Since development server can not handle two application at once.
 # added occupiedby column to Channel table in ccs.sql
+# added CrudHelper module which will slightly improve coding efficency 
+# added Car managment page 
+# now admins are able to swich betwen user and admin page
 
 
 # notes
@@ -21,9 +25,13 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ccs.db"
 db.init_app(app)
 Username = ''
+Username_role = ''
+
 exec_user = text('SELECT * FROM users')
 exec_station = text('SELECT * FROM stations')
 exec_channel = text('SELECT * FROM channels')
+exec_usercars = text('SELECT * FROM user_cars')
+exec_channel_usercars = text('SELECT * FROM channel_usercars')
 
 @app.route('/index', methods = ['POST', 'GET'])
 def user_index():
@@ -32,15 +40,17 @@ def user_index():
             case 'Order a station':
                 return redirect('/user/order_station')
             case 'car managment':
-                return redirect('/user/car_managment')
+                return redirect('/user/add_car')
+            case 'switch back to admin page':
+                return redirect('/admin/index')
             case _:
                 pass
     else:
-        return render_template('user_index.html', username=Username)
+        return render_template('user_index.html', username=Username, Username_role=Username_role)
 
 @app.route('/user/order_station', methods = ['POST', 'GET'])
 def order_station():
-    global Username, Station_list
+    global Username
     if Username == '':
         return redirect('/index')
     else:
@@ -74,11 +84,44 @@ def order_station():
         else:
             return render_template('user_order_station.html', channel=channel_list, username=Username)
 
+@app.route('/user/add_car', methods = ['POST', 'GET'])
+def add_car():
+    global Username
+    if Username == '':
+        return redirect('/index')
+    else:
+        user_id = CrudHelper.username(Username).username_to_id()
+        exec_usercars_individual = text(f'SELECT * FROM user_cars WHERE id_user = {user_id}')
+        channel_usercars_list = db.session.execute(exec_usercars_individual)
+        if request.method == 'POST':
+            match request.form['button']:
+                case 'add car':
+                    car_name = request.form['car_name']
+                    add_car = UserCar(id_user = user_id,
+                                      carname = car_name)
+                    if car_name == '':
+                        return redirect('/user/add_car')
+                    else:
+                        db.session.add(add_car)
+                        db.session.commit()
+                        return redirect('/user/add_car')
+                case 'remove car':
+                    markers = request.form.getlist("table")
+                    for i in markers:
+                        UserCar.query.filter_by(id=i).delete()
+                        db.session.commit()
+                    return redirect('/user/add_car')
+        else:
+            return render_template('user_add_car.html', channel_usercars_list=channel_usercars_list)
+
+
 
 @app.route('/admin/index', methods = ['POST', 'GET'])
 def index():
     if request.method == 'POST':
         match request.form['index_buttons']:
+            case 'switch to user account':
+                return redirect('/index')
             case 'station managment':
                 return redirect('/admin/station_managment')
             case 'channel managment':
@@ -92,7 +135,7 @@ def index():
     
 @app.route('/admin/station_managment', methods = ['POST', 'GET'])
 def station_managment():
-    global Username, Station_list
+    global Username
     if Username == '':
         return redirect('/index')
     else:
@@ -165,7 +208,7 @@ def channel_managment():
                 case _:
                     return str(request.form['button'])
         else:
-            return render_template('admin_channel_managment.html', channels=channel_list, username=Username)
+            return render_template('admin_channel_managment.html', channels=channel_list, username=Username) #channel_station_name=channel_station_name)
 
 @app.route('/admin/report_page', methods = ['POST', 'GET'])
 def report_page():
@@ -189,16 +232,18 @@ def report_page():
 def logout():
     global Username
     Username = ''
+    Username_role = ''
     return redirect('/index') 
 
 
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        global Username
+        global Username, Username_role
         Username = request.form['Username']
         Password = request.form['Password']
         find_users = Users.query.filter_by(username=Username).first()
+        Username_role = find_users.role
 
         try:
             if find_users.password == Password and find_users.role == 'user':
@@ -218,5 +263,8 @@ def login():
     else:
         return render_template('admin_login.html')
 
+@app.route('/test')
+def test():
+    stations = CrudHelper.channel_station_parrent_id_to_name()
 if __name__ == '__main__':
     app.run(debug=True)
--- src/common/model/Models.py
+++ src/common/model/Models.py
@@ -16,8 +16,9 @@ class Station(db.Model):
     channels_per_station = db.Column(db.Integer, unique=False, nullable=False)
 
 class UserCar(db.Model):
+    __tablename__ = 'user_cars'
     id = db.Column(db.Integer, primary_key=True)
-    id_user = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+    id_user = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     carname = db.Column(db.String(200), unique=False, nullable=False)
 
 class ChannelUserCar(db.Model):
